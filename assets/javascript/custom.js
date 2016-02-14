cd755b6b678899c053081691a7a856e8795ce292
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-02-14 22:21:59 +0100

    Init

---
+++ assets/javascript/custom.js
@@ -0,0 +1,136 @@
+var app = angular.module("PhotoDesigner", ['colorpicker.module', 'ui.bootstrap-slider']);
+
+app.factory('instagram', ['$http', function($http){
+	return {
+		fetchHashtag: function(hashtag, callback){
+			
+			var endPoint = "https://api.instagram.com/v1/tags/"+hashtag+"/media/recent?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
+			
+			$http.jsonp(endPoint).success(function(response){
+				callback(response.data);
+			});
+		}
+	}
+
+}]);
+
+app.directive('myEnter', function () {
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if(event.which === 13) {
+				scope.$apply(function (){
+					scope.$eval(attrs.myEnter);
+				});
+				
+				event.preventDefault();
+			}
+		});
+	};
+});
+
+app.controller('PhotoEditorController', ['$scope', 'instagram', function ($scope, instagram){
+	$scope.instagram = {};
+	$scope.instagram.pics = [];
+	$scope.instagram.query = "women";
+	$scope.instagram.selected = -1;
+
+	$scope.settings = {};
+	$scope.settings.text = "Žene UVEK tačno ZNAJU šta rade, ali NIKAD ne znaju šta HOĆE! ";
+	$scope.settings.textColor = "#ffffff";
+	$scope.settings.shadowColor = "#000000";
+	$scope.settings.fontSize = "40";
+	$scope.settings.textWidth = 0.7;
+
+	$scope.output = {};
+	$scope.output.image = "";
+
+	$scope.instagram.search = function(){
+		instagram.fetchHashtag($scope.instagram.query, function(data){
+			$scope.instagram.selected = -1;
+			$scope.instagram.pics = data;
+
+			console.log(data);
+		});
+	}
+
+	$scope.instagram.select = function(id){
+		$scope.instagram.selected = id;
+
+		$scope.rerender();
+	}
+
+	$scope.rerender = function(){
+		if($scope.instagram.selected == -1) return false;
+		
+		var width = 640;
+		var height = 640;
+
+		var fontSizePt = $scope.settings.fontSize;
+		var fontSizePx = 4 / 3 * fontSizePt;
+
+
+		var canvas = document.getElementById("image");
+
+		canvas.width = width;
+		canvas.height = height;
+
+		var context = canvas.getContext("2d");
+
+		var imageObj = new Image();
+
+		imageObj.crossOrigin = "Anonymous";
+
+		imageObj.onload = function(){
+			context.drawImage(imageObj, 0, 0, width, height);
+			context.font = fontSizePt+"pt Cambria";
+
+			var lines = $scope.getLines(context, $scope.settings.text, width*$scope.settings.textWidth);
+
+			var top = (height - lines.length * fontSizePx) / 2;
+
+			for(i = 0; i < lines.length; i++){
+				var textWidth = context.measureText(lines[i]).width;
+
+				context.fillStyle = $scope.settings.shadowColor;
+				context.fillText(lines[i], (height - textWidth) / 2 + 1, top + fontSizePx * (i + 1) + 1);
+
+				context.fillStyle = $scope.settings.textColor;
+				context.fillText(lines[i], (height - textWidth) / 2, top + fontSizePx * (i + 1));
+			}
+
+			document.getElementById("imageOut").src = canvas.toDataURL("image/png");
+		};
+		imageObj.src = "https://crossorigin.me/"+$scope.instagram.pics[$scope.instagram.selected].images.standard_resolution.url; 
+	}
+
+	$scope.getLines = function(ctx, text, maxWidth) {
+		var words = text.split(" ");
+		var lines = [];
+
+		var currentLine = words[0];
+
+		for (var i = 1; i < words.length; i++) {
+			var word = words[i];
+			var width = ctx.measureText(currentLine + " " + word).width;
+			if (width < maxWidth) {
+				currentLine += " " + word;
+			} else {
+				lines.push(currentLine);
+				currentLine = word;
+			}
+		}
+
+		if(currentLine != ""){
+			lines.push(currentLine);
+		}
+
+		return lines;
+	}
+
+	$scope.instagram.isSelected = function(id){
+		return $scope.instagram.selected == id;
+	}
+
+	$scope.instagram.search();
+}]);
+
---
+++ public/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Photo designer</title>
+
+		<link rel="stylesheet" type="text/css" href="./assets/stylesheets/stylesheet.css">
+	</head>
+	<body>
+		<div class="container" ng-app="PhotoDesigner" ng-controller="PhotoEditorController">
+			<div class="row">
+				<div class="col-md-4 settings">
+					<div class="panel panel-default">				
+						<h1>Step 1 - Settings</h1>
+
+						<textarea class="form-control" ng-model-options="{ updateOn: 'blur' }" ng-change="rerender()" rows="5" id="comment" ng-model="settings.text"></textarea>
+
+						<div class="form-group">
+							<label for="settings.textColor">Text color</label>
+							<input colorpicker ng-model="settings.textColor" name="settings.textColor" ng-change="rerender()" type="text" class="form-control">
+						</div>
+
+						<div class="form-group">
+							<label for="settings.shadowColor">Shadow color</label>
+							<input colorpicker ng-model="settings.shadowColor" name="settings.shadowColor" ng-change="rerender()" type="text" class="form-control">
+						</div>
+
+						<div class="form-group">
+							<label for="settings.fontSize">Font size</label>
+							<input ng-model="settings.fontSize" ng-model-options="{ updateOn: 'blur' }" name="settings.fontSize" ng-change="rerender()" type="text" class="form-control">
+						</div>
+						<div class="form-group">
+							<label for="settings.textWidth">Text width</label>
+							<slider ng-model="settings.textWidth" name="settings.textWidth" ng-change="rerender()" min="0.1" step="0.1" max="1.0"></slider>
+						</div>
+
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default instagram-picker">				
+						<h1>Step 2 - Pick background</h1>
+
+						<div class="input-group search">
+							<input ng-model="instagram.query" type="text" class="form-control" my-enter="instagram.search()" placeholder="Search for...">
+							<span class="input-group-btn">
+								<button class="btn btn-default" ng-click="instagram.search()" type="button">Go!</button>
+							</span>
+						</div>
+
+						<ul class="thumbnails image_picker_selector">
+
+							<li style="width:100%" ng-repeat="(id, p) in instagram.pics" ng-click="instagram.select(id)">
+								<div class="thumbnail" ng-class="{selected: instagram.isSelected(id)}">
+									<img style="width:100%;" ng-src="{{p.images.low_resolution.url}}"/>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default">				
+						<h1>Step 3 - Result</h1>
+
+						<canvas id="image" style="display: none"></canvas>
+						<img id="imageOut" src="http://placehold.it/640x640" style="width: 100%" class="thumbnail" />
+					</div>
+				</div>
+			</div>
+		</div>		
+		<script src="./assets/javascript/javascript.min.js"></script> 
+	</body>
+</html>
